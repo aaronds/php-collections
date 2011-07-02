2705a79cad422fa785b54b7b8d06796e623be07a
Aaron Shrimpton <aaronds@gmail.com>
2011-07-02 22:45:11 +0100

    Inital commit, all tests passing.

---
+++ Collection.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * A simple wrapper arround arrays to provide a more expressive syntax.
+ */
+
+class Collection {
+
+	public static function fromArray($array){
+		$collection = new Collection($array);
+		return $collection;
+	}
+
+	private function __construct($value){
+		$this->value = $value;
+	}
+
+	public function map($fn){
+		$this->value = array_map($fn,$this->value);
+
+		return $this;
+	}
+
+	public function filter($fn){
+		$this->value = array_filter($this->value,$fn);
+		return $this;
+	}
+
+	public function reduce($fn,$mem = null){
+		return array_reduce($this->value,$fn,$mem);
+	}
+
+	public function withEach($fn){
+		array_walk($this->value,$fn);
+		return $this;
+	}
+
+	public function start($fn){
+		if(count($this->value) > 0){
+			$this->value[0] = $fn($this->value[0]);
+		}
+
+		return $this;
+	}
+
+	public function end($fn){
+		$length = count($this->value);
+		if($length){
+			$this->value[$length - 1] = $fn($this->value[$length - 1]);
+		}
+
+		return $this;
+	}	
+
+	public function sort($fn){
+		usort($this->value,$fn);
+
+		return $this;
+	}
+
+	public function count(){
+		return count($this->value);
+	}
+
+	public function value(){
+		return $this->value;
+	}
+
+	public function isEmpty(){
+		return count($this->value) == 0;
+	}
+}
---
+++ CollectionSpec.php
@@ -0,0 +1,130 @@
+<?php
+
+include "Collection.php";
+
+$describe("Collection construction",function($it){
+	$it("should build from an array",function($expect){
+		$set = Collection::fromArray(array("One","Two","Three"));
+		$expect($set)->toBeTrue();
+	});
+
+});
+
+function getDefaultSet(){
+	return Collection::fromArray(array(1,2,3,4));
+}
+
+$describe("Collection instance",function($it){
+
+	$it("unboxes the value",function($expect){
+		$set = getDefaultSet();
+		$value = $set->value();
+
+		$expect($value[1])->toEqual(2);
+	});
+
+	$it("counts the members",function($expect){
+		$set = getDefaultSet();
+
+		$expect($set->count())->toEqual(4);
+	});
+
+	$it("applies a function to the element at the start",function($expect){
+		$set = getDefaultSet();
+
+		$set->start(function($val) use ($expect) {
+			$expect($val)->toEqual(1);
+			return 5;
+		});
+
+		$values = $set->value();
+
+		$expect($values[0])->toEqual(5);
+	});
+
+	$it("applies a function to the element at the end",function($expect){
+		$set = getDefaultSet();
+
+		$set->end(function($val) use ($expect){
+			$expect($val)->toEqual(4);
+			return 5;
+		});
+
+		$values = $set->value();
+
+		$expect($values[3])->toEqual(5);
+	});
+
+	$it("maps over the members",function($expect){
+		$set = getDefaultSet();
+
+		$set->map(function($val){ return $val * 2;});
+
+		$set->start(function($val) use ($expect) {
+			$expect($val)->toEqual(2);
+		});
+	});
+
+	$it("reduces to a value",function($expect){
+		$set = getDefaultSet();
+
+		$sum = $set->reduce(function($sum,$val){ return $sum + $val;});
+
+		$expect($sum)->toEqual(10);
+	});
+
+	$it("Iterates over the lot",function($expect){
+		$set = getDefaultSet();
+
+		$number = 1;
+
+		$set->withEach(function($val) use ($expect,&$number){
+			$expect($val)->toEqual($number);
+			$number++;
+		});
+	});
+
+	$it("Sorts the members",function($expect){
+		$set = getDefaultSet();
+		$set->sort(function($a,$b){
+			return $b - $a;
+		});
+
+		$set->start(function($val) use ($expect){
+			$expect($val)->toEqual(4);
+		});
+	});
+
+	$it("Can test for emptynes",function($expect){
+		$set = getDefaultSet();
+		$emptySet = Collection::fromArray(array());
+
+		$expect($set->isEmpty())->toBeFalse();
+
+		$expect($emptySet->isEmpty())->toBeTrue();
+	});
+
+	$it("Filters its elements",function($expect){
+		$set = getDefaultSet();
+
+		$set->filter(function($el){
+			return $el % 2 == 0;
+		});
+
+		$expect($set->count())->toEqual(2);
+	});
+
+	$it("Calls can be chained together",function($expect){
+		$set = getDefaultSet();
+
+		$tmp = $set->filter(function($val){
+			return $val % 2 == 0;
+		})->map(function($val){
+			return $val * 2;
+		})->reduce(function($sum,$val){
+			return $sum + $val;
+		});
+
+		$expect($tmp)->toEqual(12);
+	});
+});
